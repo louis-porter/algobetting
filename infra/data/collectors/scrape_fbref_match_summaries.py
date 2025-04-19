95f7b2284e0ca7a9dd1ad8b910d4e16cc7ef0c99
lsp2610 <louis.porter26@gmail.com>
2025-04-19 06:51:38 +0100

    more scrape

--- infra/data/collectors/scrape_fbref_match_summaries.py
+++ infra/data/collectors/scrape_fbref_match_summaries.py
@@ -604,7 +604,7 @@ class RecentMatchDataScraper:
 
 if __name__ == "__main__":
     # Set the season and number of days to look back
-    season = "2024-2025"  # Update with current season
+    season = "2023-2024"  # Update with current season
     league = "La-Liga"
     league_id = 12
     days_back = 10000  # Get matches from last 3 days
